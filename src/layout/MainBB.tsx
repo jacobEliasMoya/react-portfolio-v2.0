4cadcd59e3f76f0b349127f3c5600a89b9ad1291
Jake <jemoya1994@gmail.com>
2024-11-26 20:01:53 -0800

    started to add more animations within tailwind, unfortunately right now I dont have a great way to manage the animation delays

--- src/layout/MainBB.tsx
+++ src/layout/MainBB.tsx
@@ -7,42 +7,44 @@ import jakeCartoon from "../assets/cartoon-jm.png"
 const MainBB = () => {
     
   return (
-    <section className='transition-all w-full min-h-screen  bg-red-600 flex items-center justify-between py-4 px-8 flex-wrap flex-col'>
+    <section className='transition-all w-full min-h-screen  bg-red-600 flex items-center justify-between py-4 px-8 flex-wrap flex-col overflow-hidden'>
         <div className="w-full flex gap-8 ">
-            <div className="flex flex-col">            
-                <H1element additionalClasses={'text-4xl md:text-6xl lg:text-8xl flex flex-col text-left '} headerText={'Design'} spanClasses={'text-white -mt-5 sm:-mt-6 md:-mt-8 lg:-mt-14'} spanText={'Develop'}/>
+            <div className="flex flex-col" >            
+                <H1element additionalClasses={'text-4xl md:text-6xl lg:text-8xl flex flex-col text-left opacity-0 animate-fall relative -top-[400px]'} headerText={'Design'} spanClasses={'text-white -mt-5 sm:-mt-6 md:-mt-8 lg:-mt-14 opacity-0 animate-fallOne relative -top-[400px]'} spanText={'Develop'}/>
             </div>
-            <div className="flex flex-col gap-2 md:gap-4 w-full justify-center">
-                <div className="rounded md:rounded-xl h-1/6 bg-zinc-800 w-full"></div>
-                <div className="rounded md:rounded-xl h-2/5 lg:h-3/6 bg-white w-full"></div>
+            <div className="flex flex-col gap-2 md:gap-4  w-full justify-center ">
+                <div className="rounded md:rounded-xl h-1/6 bg-zinc-800 w-full opacity-0 animate-fallTwo relative -top-[400px]"></div>
+                <div className="rounded md:rounded-xl h-2/5 lg:h-3/6 bg-white w-full opacity-0 animate-fallThree relative -top-[400px]"></div>
             </div>
         </div>
 
         <div className="w-full text-center flex justify-between items-center flex-col md:flex-row ">
 
-            <div className="relative -bottom-10 md:-bottom-0 bg-gradient-to-tr from-zinc-950 to bg-zinc-700 rounded-full w-36 h-36 m-w-36 m-h-36 md:w-52 md:h-52 md:m-w-52 md:m-h-52 lg:w-60 lg:h-60 lg:m-w-60 lg:m-h-60"></div>
+            <div className=" animate-grow [scale:0] opacity-0 bg-gradient-to-tr from-zinc-950 to bg-zinc-700 rounded-full w-36 h-36 m-w-36 m-h-36 md:w-52 md:h-52 md:m-w-52 md:m-h-52 lg:w-60 lg:h-60 lg:m-w-60 lg:m-h-60"></div>
             
-            <div className="z-10 rounded-xl  md:rounded-3xl md:left-1/2 md:-translate-x-1/2 relative md:absolute bg-red-500    md:w-5/6 md:h-24 w-12 h-96 flex justify-center ">
-                <div className=" shadow-[0em_0.25em_rgba(0,0,0,0.15)] hover:shadow-[0em_1em_rgba(0,0,0,0.15)] group transition-all ease  bg-white rounded-full w-60 h-60 md:w-80 md:h-80 lg:w-96 lg:h-96 absolute top-1/2 -translate-y-[50%] hover:-translate-y-[calc(50%+15px)]  hover:drop-shadow-lg flex items-center justify-center ">
-                    <div className="bg-red-600 rounded-full w-48 h-48 md:w-64 md:h-64 lg:w-80 lg:h-80 flex items-center justify-center group-hover:animate-wiggle">
+            <div className="transition-all z-10 rounded-full md:left-1/2 md:-translate-x-1/2 relative md:absolute bg-red-500 md:w-[0%] md:animate-expand md:h-24 w-12 h-96 flex justify-center ">
+                
+                <div className="opacity-0 cursor-pointer animate-fallTen -top-[200%] shadow-[0em_0.25em_rgba(0,0,0,0.15)] hover:shadow-[0em_1em_rgba(0,0,0,0.15)] transition-all ease  bg-white rounded-full w-60 h-60 md:w-80 md:h-80 lg:w-96 lg:h-96 absolute -translate-y-[50%] hover:-translate-y-[calc(50%+15px)]  hover:drop-shadow-lg flex items-center justify-center ">
+                    <div className="bg-red-600 rounded-full w-48 h-48 md:w-64 md:h-64 lg:w-80 lg:h-80 flex items-center justify-center animate-wiggle">
                         <img src={jakeCartoon} alt="jake" className="w-1/2 pointer-events-none"/>
                     </div>
                 </div>
+
             </div>
 
-            <div className="relative -top-10 md:-top-0 bg-gradient-to-tr from-zinc-950 to bg-zinc-700 rounded-full w-36 h-36 m-w-36 m-h-36 md:w-52 md:h-52 md:m-w-52 md:m-h-52 lg:w-60 lg:h-60 lg:m-w-60 lg:m-h-60"></div>
+            <div className="animate-growOne [scale:0] opacity-0  bg-gradient-to-tr from-zinc-950 to bg-zinc-700 rounded-full w-36 h-36 m-w-36 m-h-36 md:w-52 md:h-52 md:m-w-52 md:m-h-52 lg:w-60 lg:h-60 lg:m-w-60 lg:m-h-60"></div>
 
         </div>
 
-        <div className="w-full flex gap-8">
+        <div className="w-full flex gap-8 ">
       
             <div className="flex flex-col gap-2 md:gap-4 w-full justify-center">
-                <div className="rounded md:rounded-xl h-1/6 bg-zinc-800 w-full"></div>
-                <div className="rounded md:rounded-xl h-2/5 lg:h-3/6 bg-white w-full"></div>
+                <div className="rounded md:rounded-xl h-1/6 bg-zinc-800 w-full opacity-0 animate-fallFour relative -top-[400px]"></div>
+                <div className="rounded md:rounded-xl h-2/5 lg:h-3/6 bg-white w-full opacity-0 animate-fallFive relative -top-[400px]"></div>
             </div>
 
             <div className="flex flex-col">            
-                <H2element additionalClasses={'text-4xl md:text-6xl lg:text-8xl flex flex-col text-right '} headerText={"Jake"} spanClasses={'text-white -mt-5 sm:-mt-6 md:-mt-8 lg:-mt-14'} spanText={'Moya'}/>
+                <H2element additionalClasses={'opacity-0 text-4xl md:text-6xl lg:text-8xl flex flex-col text-right animate-fallSix relative -top-[400px]'} headerText={"Jake"} spanClasses={'opacity-0 animate-fallSeven relative -top-[400px] text-white -mt-5 sm:-mt-6 md:-mt-8 lg:-mt-14'} spanText={'Moya'}/>
             </div>
         </div>
  
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,18 +9,66 @@ export default {
     },
     extend: {
       animation: {
-        wiggle: 'wiggle 1s ease-in-out infinite',
+        fall: 'fall .4s ease-out forwards',
+        fallOne: 'fall .4s .15s ease-out forwards',
+        fallTwo: 'fall .4s .35s ease-out forwards',
+        fallThree: 'fall .4s .55s ease-out forwards',
+        fallFour: 'fall .4s .75s ease-out forwards',
+        fallFive: 'fall .4s .95s ease-out forwards',
+        fallSix: 'fall .4s 1.05s ease-out forwards',
+        fallSeven: 'fall .4s 1.25s ease-out forwards',
+        fallEight: 'fall .4s 1.45s ease-out forwards',
+        fallNine: 'fall .4s 1.65s ease-out forwards',
+        
+        grow:'grow .2s 1.45s ease-out forwards',
+        growOne:'grow .2s 1.65s ease-out forwards',
+
+        fallTen: 'fall2 .4s 1.85s ease-out forwards',
+
+        wiggle: 'wiggle 1s 2s ease-in-out infinite',
+
+        expand: 'expand .5s 2.25s ease-in-out forwards',
       },
       keyframes: {
         wiggle: {
           '0%, 100%': { transform: 'rotate(-0deg)' },
           '50%': { transform: 'rotate(3deg)' },
-        }
+        },
+        fall: {
+          '0%,': { top: '-200px', opacity:'0' },
+          '60%': { top: '0px', opacity:'1' },
+          '70%': { top: '20px', opacity:'1' },
+          '80%': { top: '0px', opacity:'1' },
+          '90%': { top: '10px', opacity:'1' },
+          '100%': { top: '0px', opacity:'1' },
+        },
+        fall2: {
+          '0%,': { top: '-200%', opacity:'0' },
+          '60%': { top: '50%', opacity:'1' },
+          '70%': { top: '30%', opacity:'1' },
+          '80%': { top: '50%', opacity:'1' },
+          '90%': { top: '40%', opacity:'1' },
+          '100%': { top: '50%', opacity:'1' },
+        },
+        grow: {
+          '0%,': { scale: '0', opacity:'0' },
+          '60%,': { scale: '1.4', opacity:'1' },
+          '70%,': { scale: '.8', opacity:'1' },
+          '80%,': { scale: '1.2', opacity:'1' },
+          '90%,': { scale: '.9', opacity:'1' },
+          '100%': { scale: '1', opacity:'1' },
+        },
+
+        expand: {
+          '0%,': { width: '0%', opacity:'0' },
+          '100%': { width: '83.333333%', opacity:'1' },
+        },
+
       },
 
     },
   },
   plugins: [],
-  important:true
+  important:false
 }
 
