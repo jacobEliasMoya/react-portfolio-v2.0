d0405c01cbda679665d3fbbe91549746260e1e7b
Jake <jemoya1994@gmail.com>
2024-12-03 22:14:53 -0800

    fixed path, was not working properly when built/ deployed

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
 
   return (  
 
-    <div className="bg-zinc-800 font-retro text-zinc-800 w-full h-max relative cursor-default md:cursor-none">
+    <div className="bg-zinc-800 font-retro text-zinc-800 w-full h-max relative cursor-default md:cursor-none overflow-hidden">
       <MainBB />
       <SkillsSection />
       <div className="backdrop-invert  hidden md:block after:content-[''] after:w-[150%] after:h-[150%] after:absolute after:rounded-[100%] after:-translate-y-1/2 after:-translate-x-1/2 after:left-1/2 after:scale-125 after:top-1/2 after:border-4 after:border-white w-8 h-8 bg-zinc-900 bg-opacity-85  rounded-full absolute z-10 -translate-y-1/2 -translate-x-1/2 pointer-events-none" 
