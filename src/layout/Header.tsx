69e501af22ee4cee70d9048ef679f4643b18c4a0
Jake <jemoya1994@gmail.com>
2024-12-29 16:47:28 -0800

    header animations on open close, simple state

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react'
 import jakeCartoon from '../assets/favi.png'
 
 const Header = () => {
+
+  const [headerActive,setHeaderActive] = useState(false);
+
+  const handleClick = () => {
+    setHeaderActive(prev=>!prev)
+  }
+
   return (
-    <header className='hidden w-[98vw] p-3 bg-white mx-auto fixed top-2 left-1/2 -translate-x-1/2 z-40 rounded-full flex-row drop-shadow-lg '>
-        <button>
-            <img src={jakeCartoon} alt="" className='h-16 hover:scale-110 transition-all'/>
-        </button>
+    <header className={`${headerActive ? 'min-w-[calc(100%-2em)]' : 'min-w-12'} flex items-center justify-start bg-zinc-800 mx-auto fixed bottom-[1em] left-[1em]  z-40 rounded-full flex-row transition-all ease duration-200 drop-shadow border-2 border-white `}>
+      <button onClick={handleClick}>
+          <img src={jakeCartoon} alt="" className={`${headerActive ? 'scale-75' : 'scale-100'} h-12 w-12 active:scale-110 transition-all` }/>
+      </button>
+
+
+
     </header>
   )
 }
