f2397ed789022d38d1e0cca8857b7fd4c1f7e074
Jake <jemoya1994@gmail.com>
2025-01-22 22:53:36 -0800

    updated minor issue with a neg number

--- src/layout/CodingLanguages.tsx
+++ src/layout/CodingLanguages.tsx
@@ -170,7 +170,7 @@ const CodingLanguages = () => {
             <section id="coding" className="relative origin-right w-11/12 rounded-lg my-8 md:my-20 bg-white flex justify-start flex-wrap flex-col overflow-hidden mx-auto pb-7 md:pb-12 pt-6 md:pt-10 px-6 md:px-8 [box-shadow:_.5em_.5em_#960707] md:[box-shadow:_1em_1em_#960707] transition-all ease-linear duration-[.1s]"             
             
                 style={{
-                bottom:`-${animationStart && animationStart >= 0 ? animationStart : '0'}px`,
+                bottom:`${animationStart && animationStart >= 0 ? -animationStart : '0'}px`,
                     opacity:opacityStart
                 }}
             >
