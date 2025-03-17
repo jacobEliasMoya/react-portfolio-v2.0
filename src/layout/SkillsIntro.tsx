e6e014a7f92a2f2a2f169a051f8bbd6722750796
Jake <jemoya1994@gmail.com>
2025-03-16 21:37:19 -0700

    erg

--- src/layout/SkillsIntro.tsx
+++ src/layout/SkillsIntro.tsx
@@ -7,7 +7,7 @@ type Props = {
 const SkillsIntro = (props:Props) => {
 
     return (
-        <section id={props.id} className='z-4 sticky top-0 origin-bottom transition-all ease-out duration-300 w-full min-h-screen   flex items-center justify-center p-6 md:p-8 py-6 md:py-10 flex-wrap flex-col rounded-b-3xl [background-image:radial-gradient(#91060c,#210303)] bg-fixed'>
+        <section id={props.id} className='z-6 sticky top-0 origin-bottom transition-all ease-out duration-300 w-full min-h-screen   flex items-center justify-center p-6 md:p-8 py-6 md:py-10 flex-wrap flex-col rounded-b-3xl [background-image:radial-gradient(#91060c,#210303)] bg-fixed'>
             <div className="w-full text-center flex justify-center items-center flex-col md:flex-row  ">
                 <H2element additionalClasses={'text-3xl md:text-6xl lg:text-7xl flex flex-col text-center text-white [word-spacing:10px] tracking-wide'} headerText={"I get nerdy"} spanClasses={''} spanText={'View My Work'}/>
             </div>
