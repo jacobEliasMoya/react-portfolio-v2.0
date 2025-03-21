d7a6e1725b4e596f32e7da535b0a11e58a4e9ef5
jacob moya <jemoya1994@Gmail.com>
2025-03-20 23:50:09 -0700

    updated intro sections to be more bold for contract

--- src/layout/RandomQuote.tsx
+++ src/layout/RandomQuote.tsx
@@ -39,19 +39,14 @@ const RandomQuote = () => {
 
     },[])
 
-    useEffect(()=>{
-        console.log(quote)
-    },[quote])
-
-
-  return (
+    return (
         <section className='z-100 absolute bg-cover bg-center min-h-25 w-[90dvw] md:w-100 text-balance pt-4 md:pt-7 -top-20  sm:33 md:40 lg:left-55 bg-white scale-0 transition-all ease group-active:scale-100 flex-col items-start md:origin-bottom-left origin-bottom justify-center p-4 px-5 font-ultra rounded-3xl text-left'>
             <h2 className=" text-sm px-4 " ><span className="text-2xl absolute top-3 left-3">"</span> {quote?.quote.category}: {quote?.quote.author} <span className="text-2xl absolute bottom-3 right-3">"</span></h2>
 
             <h3 className="mt-3 w-full text-blue-950/30 text-left px-4 text-sm" >  
                 {quote ? quote.quote.quote : 'looking ...  '}    
             </h3>
-         </section>  
+        </section>  
     )
 }
 
