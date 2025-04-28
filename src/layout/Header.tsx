7595907c5012059c1cbc3e6836abeceb68728071
Jake <jemoya1994@gmail.com>
2025-04-27 22:13:35 -0700

    started to update my portfolio/ branch of to a new services. client tailored and centered build

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,25 +1,26 @@
-import { useEffect, useState,ReactElement } from 'react'
+import { useEffect, useState, ReactElement } from 'react'
 import { FaCode, FaHome } from 'react-icons/fa'
-import {  MdEmail } from 'react-icons/md'
-import {  HiClipboardDocumentCheck, HiQuestionMarkCircle } from 'react-icons/hi2'
+import { MdEmail } from 'react-icons/md'
+import { HiQuestionMarkCircle } from 'react-icons/hi2'
 import { GiRoundStar } from 'react-icons/gi'
 import { TiThMenu } from 'react-icons/ti'
-import resume from "../assets/JacobResume.pdf"
+import jakeCartoon from "../assets/cartoon-jm.webp"
+import H1element from '../components/headers/H1element'
 
 
 interface Nav {
   id: number,
-  name:string,
-  url:string,
+  name: string,
+  url: string,
   title: string,
   icon: ReactElement,
-  target:string|undefined,
-  additionalClasses:string 
+  target: string | undefined,
+  additionalClasses: string
 }
 
 const Header = () => {
 
-  const initialNav:Array<Nav> = [
+  const initialNav: Array<Nav> = [
     {
       id: 0,
       name: 'Home',
@@ -65,63 +66,72 @@ const Header = () => {
       additionalClasses: '',
       id: 4
     },
-    {
-      name: 'Resume',
-      url: resume,
-      title: 'View My Resume',
-      icon: <HiClipboardDocumentCheck />,
-      target: '_blank',
-      additionalClasses: 'hidden md:block',
-      id: 5
-    },
-
-
-
-
-    
   ]
 
-  const [navigationElms,setNavigationElms] = useState<Nav[]>();
-  const [headerActive,setHeaderActive] = useState<boolean>();
-  const [isActive,setIsActive ] = useState<boolean>(false);
+  const [navigationElms, setNavigationElms] = useState<Nav[]>();
+  const [headerActive, setHeaderActive] = useState<boolean>();
+  const [isActive, setIsActive] = useState<boolean>(false);
 
   const handleClick = () => {
-    setHeaderActive(prev=>!prev)
+    setHeaderActive(prev => !prev)
   }
 
   const handleMobileNav = () => {
-      window.innerWidth < 768 ? setHeaderActive(true) : setHeaderActive(false)
+    window.innerWidth < 768 ? setHeaderActive(true) : setHeaderActive(false)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     setTimeout(handleMobileNav, 975);
 
     initialNav ? setNavigationElms(initialNav) : '';
 
-    !isActive ? setTimeout(()=>setIsActive(true),550) : null;
-    
-    window.addEventListener('resize',handleMobileNav);
-  },[])
+    !isActive ? setTimeout(() => setIsActive(true), 550) : null;
+
+    window.addEventListener('resize', handleMobileNav);
+  }, [])
 
   return (
-    <header className={`${headerActive ? 'min-w-[calc(100%-1em)]  pl-6 md:pl-0 bg-opacity-85  ' : 'min-w-14 md:min-w-[65vw] lg:min-w-[55vw] '} min-h-14 text-white  flex items-center justify-center bg-blue-800/85 backdrop-blur-lg  mx-auto fixed bottom-[.5em] md:bottom-0 left-[.5em] md:left-1/2 md:-translate-x-1/2 md:rounded-b-none  z-40 rounded-full flex-row transition-all ease-out duration-300 font-ultra overflow-hidden md:overflow-visible shadow-[0em_0.25em_rgba(0,0,0,0.15)] mb-2 md:mb-0`}
+    <header className={`${headerActive ? 'min-w-[calc(100%-1em)]  pl-6 md:pl-0 bg-opacity-85' : 'w-full p-4'} min-h-10 h-max text-white  flex items-center justify-between bg-blue-800/85 backdrop-blur-lg  mx-auto fixed top-0 md:rounded-t-none rounded-4xl z-40 flex-row transition-all ease-out duration-300 overflow-hidden md:overflow-visible mb-2 md:mb-0`}
       style={{
-            bottom:`${isActive ? 0 : -200}px`,
-            opacity: isActive ? 1 : 0
-        }}
+        bottom: `${isActive ? 0 : -200}px`,
+        opacity: isActive ? 1 : 0
+      }}
     >
 
       <button aria-label="mobile-navigation-toggle" onClick={window.innerWidth < 768 ? handleClick : undefined} className='absolute left-0 md:hidden z-10 ' >
-          <TiThMenu className={`${headerActive ? 'scale-75' : 'scale-100'} rounded-full text-white p-2 border-8 border-white bg-blue-500 h-14 w-14 active:scale-110 transition-all` }/>
+        <TiThMenu className={`${headerActive ? 'scale-75' : 'scale-100'} rounded-full text-white p-2 border-8 border-white bg-blue-500 h-14 w-14 active:scale-110 transition-all`} />
       </button>
 
-      <ul className={` flex justify-center items-center gap-6 w-full max-w-0 `}>
-        { navigationElms?.map((item)=>(
-          <li key={`nav-${item.id}`} className="cursor-none text-2xl tracking-widest lg:text-3xl group ">
-            <a aria-label={item.title} href={item.url} target={item.target ? item.target : '' } className={`flex flex-row items-center justify-center gap-2 md:gap-4 md:p-4 relative cursor-none ${item.additionalClasses}`}>{item.icon} <span className='hidden transition-all ease-out duration-200 -top-2 group-hover:-top-14 opacity-0 group-hover:opacity-100 group-hover:text-2xl  md:absolute md:inline text-sm bg-red-800 backdrop-blur-lg p-4 rounded-md '>{item.name}</span> </a>
+      <div className="flex flex-row gap-4 align-items-center justify-start w-1/2">
+
+        <div className="transition-all ease  bg-white  rounded-full min-w-18 min-h-18 relative md:hover:-translate-y-[15px] flex justify-center items-center z-10 group">
+          <div className=" rounded-full w-16 h-16 flex items-center justify-center  relative group ">
+            <img src={jakeCartoon} alt="jake" width="150" height="220" className="w-1/2 pointer-events-none select-none" />
+          </div>
+        </div>
+
+        <H1element additionalClasses={'text-5xl flex flex-row gap-2 items-center justify-center text-white '} headerText={'True'} spanClasses={''} spanText={'Pixel'} />
+
+      </div>
+
+      <ul className="flex gap-2 justify-end items-center w-1/2">
+        {navigationElms?.map((item) => (
+          <li key={`nav-${item.id}`} className="first:bg-orange-500  group cursor-none text-xl rounded-full overflow-hidden hover:bg-blue-950 ">
+            <a
+              aria-label={item.title}
+              href={item.url}
+              target={item.target ? item.target : ''}
+              className={` flex text-lg flex-row items-center justify-center  backdrop-blur-xs p-2 min-w-30 relative cursor-none transition-all ease-out duration-300  text-white ${item.additionalClasses}`}
+            >
+              <span className="hidden">{item.icon}</span>
+              <span className="transition-all ease-out duration-300">
+                {item.name}
+              </span>
+            </a>
           </li>
-        )) }  
+        ))}
       </ul>
+
     </header>
   )
 }
--- src/layout/SkillsIntro.tsx
+++ src/layout/SkillsIntro.tsx
@@ -7,9 +7,9 @@ type Props = {
 const SkillsIntro = (props:Props) => {
 
     return (
-        <section id={props.id} className='-z-1 sticky top-0 origin-bottom transition-all ease-out duration-300 w-full min-h-screen   flex items-center justify-center p-6 md:p-8 py-6 md:py-10 flex-wrap flex-col rounded-b-3xl bg-blue-950 '>
+        <section id={props.id} className='-z-1 sticky top-0 origin-bottom transition-all ease-out duration-300 w-full min-h-screen   flex items-center justify-center p-6 md:p-8 py-6 md:py-10 flex-wrap flex-col rounded-b-3xl bg-gradient-to-b from-blue-400 to-blue-600'>
             <div className="w-full text-center flex justify-center items-center flex-col md:flex-row  ">
-                <H2element additionalClasses={'text-3xl md:text-6xl lg:text-7xl flex flex-col text-center text-white [word-spacing:10px] tracking-wide'} headerText={"I get nerdy"} spanClasses={''} spanText={'View My Work'}/>
+                <H2element additionalClasses={'capitalize text-3xl md:text-6xl lg:text-7xl flex flex-col text-center text-white '} headerText={"I get nerdy"} spanClasses={''} spanText={'View My Work'}/>
             </div>
         </section>
 
